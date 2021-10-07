cd26f8e739dac2940b9b12fb3ef055ee0e170f40
Senen Rodenas <71589700+Senen1997@users.noreply.github.com>
2021-10-07 10:47:15 +0200

    No funciona e.l props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+
     return (
-        <div>
-            <div>March 28th 2021</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
+        <div className="expense-item">
+            <div> { props.Date } </div>
+            <div className="expense-item__description">
+                <h2>{props.}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
